6c8d8c239f5217497b182903aeb5ad822294ecc1
David <david99900@gmail.com>
2023-04-02 19:09:11 +0800

    fix socket is undefined issue

--- src/app/api/socket/route.ts
+++ src/app/api/socket/route.ts
@@ -2,7 +2,7 @@ import { Server } from 'socket.io'
 import { SocketRequest, SocketResponse } from 'src/typing/socket'
 
 export function GET(req: SocketRequest, resp: SocketResponse) {
-    if (req.socket.server.io) {
+    if (req.socket && req.socket.server.io) {
         return new Response(JSON.stringify({ message: 'Socket.io is already running' }), {
             status: 200
         })
@@ -11,13 +11,10 @@ export function GET(req: SocketRequest, resp: SocketResponse) {
     const io = new Server(resp.socket.server)
 
     io.on('connection', (socket) => {
-        socket.id
-        socket.on('message', (message) => {
-            socket.emit('message', message)
-        })
+        
     })
 
     resp.socket.server.io = io
 
-    return new Response(JSON.stringify({ message: 'Socket.io is running' }))
+    return new Response(JSON.stringify({ message: 'Socket.io is running' }),)
 }
